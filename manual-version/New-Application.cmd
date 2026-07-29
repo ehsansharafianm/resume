eff950f36a72d78b17ae90e898d89e8dc04dccb4
Ehsan Sharafian <ehsan.sharafian.m@gmail.com>
2026-07-29 13:14:10 -0400

    Restore separate resume launchers (#7)

--- manual-version/New-Application.cmd
+++ manual-version/New-Application.cmd
@@ -1,9 +1,7 @@
 @echo off
 python "%~dp0system\resume_tool.py" migrate --quiet
 if "%~1"=="" cls
-if "%~1"=="" python "%~dp0system\resume_tool.py" menu
+if "%~1"=="" python "%~dp0system\resume_tool.py" wizard
 if "%~1"=="" exit /b %errorlevel%
-if /i "%~1"=="master" python "%~dp0system\resume_tool.py" %*
-if /i "%~1"=="master" exit /b %errorlevel%
 python "%~dp0system\resume_tool.py" create %*
 exit /b %errorlevel%
